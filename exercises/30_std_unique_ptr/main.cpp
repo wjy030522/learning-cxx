97c6b642b63b1c623116a15d67667398e86395d7
wjy <1357253688@qq.com>
2024-12-18 16:53:32 +0800

    0

--- exercises/30_std_unique_ptr/main.cpp
+++ exercises/30_std_unique_ptr/main.cpp
@@ -57,8 +57,8 @@ int main(int argc, char **argv) {
     std::vector<const char *> answers[]{
         {"fd"},
         // TODO: 分析 problems[1] 中资源的生命周期，将记录填入 `std::vector`
-        {"ffr","d"},
-        {"r","d","d"},
+        {"d","ffr"},
+        {"d","d","r"},
     };
 
     std::cout << "Problems[0]: ";
