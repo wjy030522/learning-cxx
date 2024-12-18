6e6372f9c9e6fab5b3d2f7162328474c55651f77
wjy <1357253688@qq.com>
2024-12-18 15:25:02 +0800

    0

--- exercises/33_std_accumulate/main.cpp
+++ exercises/33_std_accumulate/main.cpp
@@ -1,21 +1,21 @@
-﻿#include "../exercise.h"
-#include <numeric>
+﻿// #include "../exercise.h"
+// #include <numeric>
 
-// READ: `std::accumulate` <https://zh.cppreference.com/w/cpp/algorithm/accumulate>
+// // READ: `std::accumulate` <https://zh.cppreference.com/w/cpp/algorithm/accumulate>
 
-int main(int argc, char **argv) {
-    using DataType = float;
-    int shape[]{1, 3, 224, 224};
-    // TODO: 调用 `std::accumulate` 计算：
-    //       - 数据类型为 float；
-    //       - 形状为 shape；
-    //       - 连续存储；
-    //       的张量占用的字节数
-    // int size =
-    int size = std::accumulate(std::begin(shape), std::end(shape), 1, std::multiplies<int>());
+// int main(int argc, char **argv) {
+//     using DataType = float;
+//     int shape[]{1, 3, 224, 224};
+//     // TODO: 调用 `std::accumulate` 计算：
+//     //       - 数据类型为 float；
+//     //       - 形状为 shape；
+//     //       - 连续存储；
+//     //       的张量占用的字节数
+//     // int size =
+//     int size = std::accumulate(std::begin(shape), std::end(shape), 1, std::multiplies<int>());
 
-    // 计算字节数
-    size *= sizeof(DataType);
-    ASSERT(size == 602112, "4x1x3x224x224 = 602112");
-    return 0;
-}
+//     // 计算字节数
+//     size *= sizeof(DataType);
+//     ASSERT(size == 602112, "4x1x3x224x224 = 602112");
+//     return 0;
+// }
