c2d9b9c57fffe1200c645d7eb2dbc8b3cf2f4c30
wjy <1357253688@qq.com>
2024-12-18 15:29:21 +0800

    0

--- exercises/00_hello_world/main.cpp
+++ exercises/00_hello_world/main.cpp
@@ -3,10 +3,9 @@
 // READ: std streams <https://zh.cppreference.com/w/cpp/io/c/std_streams>
 // READ: 流修饰符 <https://zh.cppreference.com/w/cpp/io/manip>
 // READ: format in cxx20 <https://zh.cppreference.com/w/cpp/utility/format/format>
-#include<iostream>
-using namespace std;
+
 int main(int argc, char **argv) {
     // TODO: 在控制台输出 "Hello, InfiniTensor!" 并换行
-    cout<<"Hello, InfiniTensor!"<<endl;
+    std::cout<<"Hello, InfiniTensor!"<<std::endl;
     return 0;
 }
